6959c64aa9544936ab8b81d66fd62341e2f60935
SheloSett <shelosetton@gmail.com>
2025-12-01 21:13:13 -0300

    unas cositas mas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import ProductRouter from './router/product.router.js'
 import ProductAdminRouter from './router/product.admin.router.js'
+import morgan from 'morgan'
 
 
 const server = express()
 server.use(express.json())
+server.use(morgan)
 server.use("/api/v1", ProductRouter)
 server.use("/api/v1", ProductAdminRouter)
 
